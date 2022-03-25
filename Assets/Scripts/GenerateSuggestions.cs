4b93e916e761a1c4b54c1b6a2482c3bc647b9af2
Sachin Bajpai <sachinbajpai@playsimple.in>
2022-03-25 12:17:50 +0530

    prefabs are now instantiating in apk as well
    earlier there was no build script in addressables group option which was the reason prefabs were not instantiating

--- Assets/Scripts/GenerateSuggestions.cs
+++ Assets/Scripts/GenerateSuggestions.cs
@@ -1,6 +1,8 @@
 using System.Collections.Generic;
 using System.IO;
+using System.Threading.Tasks;
 using Newtonsoft.Json;
+using TMPro;
 using UnityEngine;
 using UnityEngine.AddressableAssets;
 
@@ -16,18 +18,18 @@ public class GenerateSuggestions : MonoBehaviour
     public List<SuggesteduserData> suggestionlist;
     async void Start()
     {
-        GetData();
-        List<SuggestedUserScript> list = new List<SuggestedUserScript>(10);
+        await GetData();
         for (int i = 0; i < 10; i++)
         {
-            var suggeesteduser = await SuggestedUserReference.InstantiateAsync(transform).Task;
-            var suggestedUserScript = suggeesteduser.GetComponent<SuggestedUserScript>();
+            var suggesteduser = await SuggestedUserReference.InstantiateAsync(transform).Task;
+            var suggestedUserScript = suggesteduser.GetComponent<SuggestedUserScript>();
             suggestedUserScript.setter(suggestionlist[i]);
         }
     }
-    private void GetData()
+    private async Task GetData()
     {
-        string json = File.ReadAllText(Application.dataPath + "/Data/SuggestedUserData.json");
-        suggestionlist = JsonConvert.DeserializeObject<List<SuggesteduserData>>(json);
+        
+        var file = await Addressables.LoadAssetAsync<TextAsset>("Data/SuggestedUserData").Task;
+        suggestionlist = JsonConvert.DeserializeObject<List<SuggesteduserData>>(file.ToString());
     }
 }
